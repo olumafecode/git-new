69e27a951c414c1201524fcd42093f1beffbdea9
olumafecode <olumafe.code@gmail.com>
2025-12-01 23:46:40 -0600

    good

---
+++ VSC_Alarm/alarm_1.js
@@ -0,0 +1,9 @@
+let time = 30
+const message = "Paulo"
+for ( let i = 0; i <= time; i++) {
+    if (i == time) {
+        alert(message + ", wake up in " + time + " minutes!")
+    } else {
+        console.log(i)
+    }
+}
