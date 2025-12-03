8660531e21f816626ceaa0a05e5c3cc24976989b
olumafecode <olumafe.code@gmail.com>
2025-12-03 09:53:34 -0600

    ok

---
+++ VSC_Calculator/calculator.js
@@ -0,0 +1,10 @@
+function add (x,y) {
+    return(x+y);
+}
+add(3,5);
+
+prompt("Type in a number.")
+prompt("Type in another number.")
+let num1 = (prompt("Type in a number."))
+let num2 = (prompt("Type in another number."))
+alert("The sum is " + add(num1, num2));
