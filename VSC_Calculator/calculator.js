f4e2d55589962a86007f60901b5c89f8e3ec6eac
olumafecode <olumafe.code@gmail.com>
2025-12-03 18:11:03 -0600

    fixed

--- VSC_Calculator/calculator.js
+++ VSC_Calculator/calculator.js
@@ -7,14 +7,14 @@
 //alert("The sum is " + add(num1, num2));
 const passing = 75;
 function getCurrent () {
-    return parseInt(prompt("Type in your current score."));
+    return parseInt(prompt("Type in your current grade."));
 }
 function getWeight () {
     return parseInt(prompt("Type in the weight of the final exam."));
 }
 function calculate (c, w) {
-    let finalScore = (passing - (1-(w/100))*c)/(w/100)
+    let finalScore = ((passing - (1-(w/100))*c)/(w/100))
     let rounded = Math.round(finalScore);
-    return "You must get a score of ${rounded}% on the to pass this course.";
+    return `You must get a score of ${rounded}% on the final exam to pass this course.`;
 }
-alert(calculate(getCurrent(), getWeight()));
+alert( calculate(getCurrent(), getWeight()));
